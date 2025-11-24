ce7a7363fdb47cdde29c858cbb188cb6370653a0
Eduardo <eduardovcn74@gmail.com>
2025-11-24 15:34:52 -0300

    feat: Implementando o projeto base com SpringBoot

---
+++ src/test/java/com/crud/backend_users/BackendUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.backend_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
