bf821778cdd488b421daa6ed93b575e25af0e8fb
Eduardo <eduardovcn74@gmail.com>
2025-11-27 16:21:47 -0300

    feat: UsuarioRepository implementado.

---
+++ src/main/java/com/crud/backend_users/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.crud.backend_users.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.crud.backend_users.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+
+}
